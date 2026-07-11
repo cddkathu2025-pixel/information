ad5c9137576ef2a38a50ab74f2301651be859986
ชื่อของคุณบนGitHub <GitHub>
2026-07-11 19:48:42 +0700

    Add error alert to saveToFirebase

--- src/db.js
+++ src/db.js
@@ -24,7 +24,12 @@ const getFromFirebaseCache = (key) => {
 
 const saveToFirebase = (key, value) => {
   firebaseCache[key] = value;
-  setDoc(DOC_REF, { [key]: value }, { merge: true }).catch(console.error);
+  setDoc(DOC_REF, { [key]: value }, { merge: true })
+    .then(() => console.log('Successfully saved ' + key + ' to Firestore.'))
+    .catch(e => {
+      console.error("Firebase save error", e);
+      alert("ไม่สามารถบันทึกข้อมูลได้ กรุณาตรวจสอบอินเทอร์เน็ตหรือติดต่อผู้ดูแลระบบ (Error: " + e.message + ")");
+    });
 };
 
 const removeFromFirebaseCache = (key) => {
